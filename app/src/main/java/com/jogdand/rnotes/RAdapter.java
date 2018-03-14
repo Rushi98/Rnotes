48ea010f4fc19982b367aee5ce255ec6da379db9
rushi98 <rushikeshjogdand1@gmail.com>
2018-03-14 20:13:35 +0530

    fixed: notes not updating properly

--- app/src/main/java/com/jogdand/rnotes/RAdapter.java
+++ app/src/main/java/com/jogdand/rnotes/RAdapter.java
@@ -2,6 +2,7 @@ package com.jogdand.rnotes;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,20 +13,37 @@ import io.realm.Realm;
 import io.realm.RealmChangeListener;
 import io.realm.RealmQuery;
 import io.realm.RealmResults;
+import io.realm.Sort;
 
 /**
  * @author Rushikesh Jogdand.
  */
 
-@SuppressWarnings("WeakerAccess")
+@SuppressWarnings({"WeakerAccess", "FieldCanBeLocal"})
 public class RAdapter extends RecyclerView.Adapter<NotesViewHolder> implements RealmChangeListener<RealmResults<Note>>{
 
     private ArrayList<Note> notes;
+
     private boolean showArchived = false;
+    /** _as a thumb rule_ we want realm instances to be available strictly where needed
+     * see last comment of {@link #loadData()} for more
+     */
+    private Realm db;
+    private RealmQuery<Note> notesQuery;
+    private RealmResults<Note> notesResult;
+
 
     public RAdapter() {
         this.notes = new ArrayList<>();
+        db = Realm.getDefaultInstance();
         loadData();
+    }
+
+    @Override
+    public void onDetachedFromRecyclerView(@NonNull RecyclerView recyclerView) {
+        if (!db.isClosed()) db.close();
+        Log.d("Realm Status", "onDetachedFromRecyclerView: closed");
+        super.onDetachedFromRecyclerView(recyclerView);
     }
 
     public void setShowArchived(boolean show) {
@@ -33,25 +51,22 @@ public class RAdapter extends RecyclerView.Adapter<NotesViewHolder> implements R
     }
 
     public void loadData() {
-
-        Realm db = Realm.getDefaultInstance();
-        RealmQuery<Note> query = db.where(Note.class);
-        if (!showArchived) {
-            query.equalTo("isArchived", false);
-        }
-        /* A query object doesn;t containd any data't */
-        final RealmResults<Note> results = query.findAll();
-        /* Results of `query` are now stored in `results variable` */
-        results.addChangeListener(this);
-
-        this.notes = new ArrayList<>();
+        notesQuery = db.where(Note.class);
+        if (!showArchived) notesQuery = notesQuery.equalTo("isArchived", false);
+        if (notesResult != null) notesResult.removeAllChangeListeners();    // to avoid listener code running multiple times
+        notesResult = notesQuery.findAll().sort("date", Sort.DESCENDING);
+        notes.clear();
         for (Note n :
-                results) {
+                notesResult) {
             notes.add(db.copyFromRealm(n));
         }
-
-        db.close();
+        notesResult.addChangeListener(this);
         notifyDataSetChanged();
+        /** db.close();       <-- this will close all references related to db
+                                including `notesResult`
+                                but we want to listen notesResult while this recyclerView is visible
+                                so make db available for whole adapter life. see {@link Realm#close()};
+        */
     }
 
     @NonNull
@@ -73,14 +88,12 @@ public class RAdapter extends RecyclerView.Adapter<NotesViewHolder> implements R
     }
 
     @Override
-    public void onChange(@NonNull RealmResults<Note> notes) {
-        Realm db = Realm.getDefaultInstance();
-        this.notes = new ArrayList<>();
+    public void onChange(@NonNull RealmResults<Note> notesResult) {
+        notes.clear();
         for (Note n :
-                notes) {
-            this.notes.add(db.copyFromRealm(n));
+                notesResult) {
+            notes.add(db.copyFromRealm(n));
         }
-        db.close();
         notifyDataSetChanged();
     }
 }
